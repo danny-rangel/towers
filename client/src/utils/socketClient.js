632e4d09906c4dcd175938bd28c2cc0e0ac0f2a5
Danny Rangel <danielrangel.tech@gmail.com>
2019-04-04 01:35:53 -0700

    still trying

--- client/src/utils/socketClient.js
+++ client/src/utils/socketClient.js
@@ -3,9 +3,9 @@ import io from 'socket.io-client';
 let URL = "";
 
 if (process.env.NODE_ENV === 'production') {
-    URL = `https://towersmusic.io/${process.env.PORT}`;
+    URL = `https://towersmusic.io:${process.env.PORT}`;
   } else if (process.env.NODE_ENV !== 'production') {
-    URL = 'http://localhost:5000';
+    URL = '/';
   }
 
 const socket = io.connect(URL);
--- index.js
+++ index.js
@@ -35,14 +35,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
 
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT);
@@ -70,3 +63,13 @@ io.use(passportSocketIo.authorize({
     console.log('Connected!', socket.id, socket.request.user.username);
     socket.join(socket.request.user._id);
 });
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
