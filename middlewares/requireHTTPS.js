90a9febefc8032ae77e1d39a5f85ecc7ac76f62f
Danny Rangel <danielrangel.tech@gmail.com>
2019-04-13 19:27:02 -0700

    https middleware try # 324124

--- middlewares/requireHTTPS.js
+++ middlewares/requireHTTPS.js
@@ -1,6 +1,9 @@
 module.exports = (req, res, next) => {
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-      return res.redirect('https://' + req.get('host') + req.url);
+    const xfp = req.headers["X-Forwarded-Proto"] || req.headers["x-forwarded-proto"];
+    if (xfp === "http") {
+        const secureUrl = `https://${req.headers.hostname}${req.url}`;
+        res.redirect(301, secureUrl);
+    } else {
+        next();
     }
-    next();
   }
