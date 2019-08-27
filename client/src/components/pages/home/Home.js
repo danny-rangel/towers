d3e7cbb33f7eeb5b33214edda43ba4d151bee185
Danny Rangel <danielrangel.tech@gmail.com>
2019-08-26 22:10:33 -0700

    removed old code in home

--- client/src/components/pages/home/Home.js
+++ client/src/components/pages/home/Home.js
@@ -3,7 +3,7 @@ import './Home.css';
 import history from '../../../history';
 import PostList from '../../post/PostList';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import {
     fetchFollowerPosts,
     isFetching,
@@ -69,52 +69,6 @@ const Home = ({
             )}
         </Wrapper>
     );
-
-    // if (fetching || !posts) {
-    //     return <div>Loading...</div>;
-    // } else if (posts.length === 0) {
-    //     return (
-    //         <div style={{ margin: '200px auto' }}>
-    //             <h2 style={{ textAlign: 'center', color: 'white' }}>
-    //                 <b>Seems a little lonely in here.</b>
-    //                 <br></br>
-    //                 <b>
-    //                     Want to
-    //                     <Link
-    //                         to="/search/users"
-    //                         style={{
-    //                             textDecoration: 'underline',
-    //                             color: 'white'
-    //                         }}
-    //                     >
-    //                         search
-    //                     </Link>
-    //                     for some friends?
-    //                 </b>
-    //             </h2>
-    //         </div>
-    //     );
-    // }
-    // return (
-    //     <div>
-    //         <InfiniteScroll
-    //             dataLength={posts.length}
-    //             next={() => {
-    //                 continuefetchFollowerPosts(page + 1, 20);
-    //                 setPage(page + 1);
-    //             }}
-    //             hasMore={posts.length !== postCount}
-    //             loader={<div>Loading...</div>}
-    //             endMessage={
-    //                 <h3 style={{ textAlign: 'center', color: 'black' }}>
-    //                     <b>You're all caught up!</b>
-    //                 </h3>
-    //             }
-    //         >
-    //             <PostList posts={posts} />
-    //         </InfiniteScroll>
-    //     </div>
-    // );
 };
 
 const mapStateToProps = ({ posts, auth, fetching, postCount }) => {
